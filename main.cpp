7238da4d6b4d00a17d1f3f74a7a638d2c6778867
Dilan <diporrasc@unal.edu.co>
2024-12-13 19:22:49 -0500

    Works

--- main.cpp
+++ main.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 
+#include "Stack.hpp"
+
+static void bugPlanning(Stack<int>& stack)
+{
+	size_t n = stack.capacity();
+	int value;
+
+	std::cin >> value;
+	stack.push(value); // Fisrt value always goes to the stack
+
+	for (size_t i = 0; i < n - 1; ++i)
+	{
+		std::cin >> value;
+
+		if (value > stack.top()) // Add the value to the stack only if it is greater than the last
+			stack.push(value);
+	}
+
+	// Print result
+	std::cout << stack.size() << std::endl;
+	stack.print();
+}
+
 
 
 int main(int argc, const char* argv[])
 {
-	return system("curl parrot.live");
+	try {
+		size_t n;
+		while (std::cin >> n)
+		{
+			Stack<int> stack(n);
+			bugPlanning(stack);
+		}
+	}
+	catch (...) {
+		return 0;
+	}
+	
 }
